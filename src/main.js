9fb5396c027a0cc2b2a346f579cc5ca38960b072
r17n <4716989+17number@users.noreply.github.com>
2020-01-17 12:00:16 +0900

    feat: receving fcm

--- src/main.js
+++ src/main.js
@@ -6,3 +6,65 @@ Vue.config.productionTip = false;
 new Vue({
   render: h => h(App)
 }).$mount("#app");
+
+let cordova = undefined;
+let FirebasePlugin = undefined;
+const onDeviceReady = () => {
+  console.log("deviceready");
+  FirebasePlugin = window.FirebasePlugin;
+  cordova = window.cordova;
+
+  FirebasePlugin.onMessageReceived(onFcmReceived, onFcmReceivedError);
+  checkNotificationPermission(false);
+
+  console.log({ platformId: cordova.platformId });
+  switch (cordova.platformId) {
+    case "android":
+      break;
+    case "ios":
+      break;
+    default:
+      break;
+  }
+};
+
+// メッセージ受信
+const onFcmReceived = message => {
+  console.log("onMessageReceived");
+  console.log({ message });
+  // messageType, tap などに応じて処理分岐できる
+};
+const onFcmReceivedError = error => {
+  console.log("onMessageReceived");
+  console.error({ error });
+};
+
+// プッシュ通知の権限 確認/要求
+const checkNotificationPermission = requested => {
+  FirebasePlugin.hasPermission(hasPermission => {
+    if (hasPermission) {
+      // プッシュ通知の権限 あり
+      console.log("Remote notifications permission granted");
+      // トークンの取得
+      getToken();
+    } else if (!requested) {
+      // プッシュ通知の権限 要求
+      console.log("Requesting remote notifications permission");
+      FirebasePlugin.grantPermission(
+        checkNotificationPermission.bind(this, true)
+      );
+    } else {
+      console.error("Notifications won't be shown as permission is denied");
+    }
+  });
+};
+
+// トークンの取得
+const getToken = () => {
+  FirebasePlugin.getToken(
+    token => console.log("Got FCM token: " + token),
+    error => console.error("Failed to get FCM token", error)
+  );
+};
+
+document.addEventListener("deviceready", onDeviceReady);
